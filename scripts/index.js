41400b6edee55e9a148c0e1e9bf7dcf7a837f9e1
nlog <nlog675@gmail.com>
2022-06-17 23:00:25 +0300

    fix popup positioning after adding input errors

--- scripts/index.js
+++ scripts/index.js
@@ -109,13 +109,7 @@ function submitForm(e) {
 
 const formInput = document.querySelector('.popup__input');
 const formError = formElement.querySelector(`.${formInput.id}-error`);
-const isValid = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
+
 
 const hasInvalidInput = inputList => {
   return inputList.some((inputElement) => {
@@ -136,6 +130,7 @@ const toggleButtonState = (inputList, buttonElement) => {
 const setEventListeners = formElement => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
   const buttonElement = formElement.querySelector('.popup__button');
+  toggleButtonState(inputList,buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       isValid(formElement, inputElement);
@@ -172,6 +167,33 @@ const hideInputError = (formElement, inputElement) => {
   errorElement.classList.remove('popup__input_type_error');
   errorElement.textContent = '';
 };
+
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const body = document.querySelector('.page')
+//закрытие попапа на еск
+popups.forEach((popup) => {
+  body.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      closePopup(popup)
+    }
+  })
+})
+
+popups.forEach((popup) => {
+  popup.addEventListener('click', e => {
+    if (e.target === e.currentTarget) {
+      closePopup(popup)
+    }
+  })
+})
+
 
 
 
