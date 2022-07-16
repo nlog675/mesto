38cf5943ef96b14f16cfba1a1bf6cab47cb3f70c
nlog <nlog675@gmail.com>
2022-07-16 13:48:53 +0300

    create new class Section

---
+++ scripts/Section.js
@@ -0,0 +1,17 @@
+class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._renderedItems = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this.renderItems.forEach((item) => {
+      this._renderer(item);
+    })
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+}
