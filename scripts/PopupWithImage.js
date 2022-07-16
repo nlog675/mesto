2921f5f319b3618dfabef77bd795d3a3886d5379
nlog <nlog675@gmail.com>
2022-07-16 16:50:38 +0300

    create new class PopupWithImage

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,19 @@
+class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    this._popupSelector = super.popupSelector;
+    this._image = popupSelector.querySelector('.popup__image');
+    this._name = popupSelector.querySelector('.popup__caption');
+  }
+
+  open(link, name) {
+    this._image.src = link;
+    this._image.alt = name;
+    this._name.textContent = name;
+    this._popupSelector.classList.add('popup_isOpen');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  setEventListeners() {
+    super.setEventListeners()
+  }
+}
